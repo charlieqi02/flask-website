8d8f787593d021f323ddc161ada073416bfe2e31
Charlie <qi.2730028653@gmail.com>
2023-03-31 22:32:31 +0800

    Add flask app to do some basic things
    Some basic things we can do:
    - app.py can show the index page,
                 deal with the submit,
                 connect the database,
                 store info
                 and return successful page

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+
+app = Flask(__name__, template_folder='templates')
+
+# connect to mysql
+conn = mysql.connector.connect(
+    user = "root",
+    password = "1q2w3e4r",
+    host = "localhost",
+    database = 'personal_info',
+    auth_plugin = 'mysql_native_password'
+)
+
+# create user table
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS personal_info(name VARCHAR(255), gender VARCHAR(255), age INT)")
+cursor.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit/success')
+def success():
+    return "<h1> information has successfully stored! </h1>"
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    gender = request.form['gender']
+    age = request.form['age']
+
+    # inser user info
+    cursor = conn.cursor()
+    cursor.execute(f'INSERT INTO personal_info VALUE ("{name}","{gender}",{age})')
+    conn.commit()
+    cursor.close()
+
+    return redirect(url_for('success'))
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
