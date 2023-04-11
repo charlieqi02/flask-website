7f0b961649aa8680025056ee2c15ae774bddf140
Charlie <charlieqi02@gmail.com>
2023-04-11 23:19:05 +0800

    Change the module we use to connect mysql Add id in table personal_info

--- app.py
+++ app.py
@@ -1,32 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+# config URL for database
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:1q2w3e4r@localhost:3306/flask_website"
+db = SQLAlchemy(app)
 
-# connect to mysql
-conn = mysql.connector.connect(
-    user = "root",
-    password = "1q2w3e4r",
-    host = "localhost",
-    database = 'flask_website',
-    auth_plugin = 'mysql_native_password'
-)
 
-# create user table
-cursor = conn.cursor()
-# add primary key to this table in order not to store the same info
-cursor.execute("CREATE TABLE IF NOT EXISTS \
-                personal_info(\
-                  name      VARCHAR(255), \
-                  gender    VARCHAR(255), \
-                  age       INT, \
-                  PRIMARY KEY (name, gender, age));") 
-cursor.close()
+class PersonalInfo(db.Model):
+    __name__ = 'per_infotest'
+    id = db.Column(db.String(15), primary_key=True)
+    name = db.Column(db.String(75))
+    gender = db.Column(db.String(10))
+    age = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<PersonalInfoTest id>'
 
 
 @app.route('/')
 def index():
+    db.create_all()
     return render_template('index.html')
 
 
@@ -37,22 +32,13 @@ def success():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
-    gender = request.form['gender']
-    age = request.form['age']
-
-    # inser user info
-    cursor = conn.cursor()
-    # keep table clean and simple
-    cursor.execute(f'INSERT IGNORE INTO personal_info VALUE ("{name}","{gender}",{age})')
-    conn.commit()
-    cursor.close()
+    info = PersonalInfo(
+        id=request.form['id'],
+        name=request.form['name'],
+        gender=request.form['gender'],
+        age=request.form['age']
+    )
+    db.session.add(info)
+    db.session.commit()
 
     return redirect(url_for('success'))
-
-
-
-
-
-if __name__ == "__main__":
-    app.run()
