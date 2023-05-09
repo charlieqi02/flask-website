ee628a0271bf8f6e773de304413678dc0b87e3ad
Charlie <charlieqi02@gmail.com>
2023-05-09 22:15:02 +0800

    Add Advanced calculator to calculate calculus problems

--- calculator/app.py
+++ calculator/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
-import sympy
+from flask import Flask, render_template, request, send_file
+from latex2sympy2 import latex2latex
 
 
 app = Flask(__name__)
@@ -9,10 +9,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/simple')
 def simple():
     return render_template('simple.html')
 
-@app.route('/advanced')
+
+# example: "\frac{d}{dx} \int^{x}_{0} f(x) dx"
+@app.route('/advanced', methods=['GET', 'POST'])
 def advanced():
+    if request.method == 'POST':
+        input_exps = request.form['expression']
+        output_exps = latex2latex(input_exps)
+        return render_template('advanced.html', 
+                    input=f"\[ {input_exps} \]", output=f"\[ {output_exps} \]")
+
     return render_template('advanced.html')
